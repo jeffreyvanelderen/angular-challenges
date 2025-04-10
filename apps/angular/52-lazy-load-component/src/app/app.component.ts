4df35b89d3dd1a3680d1bd66568d53d40993ee9e
JeffreyVanelderen <jeffrey.vanelderen@axxes.com>
2025-04-10 11:55:50 +0200

    52: Refactor lazy loaded component to use defer + use standalone instead of module based;

--- apps/angular/52-lazy-load-component/src/app/app.component.ts
+++ apps/angular/52-lazy-load-component/src/app/app.component.ts
@@ -1,22 +1,27 @@
 import { Component, signal } from '@angular/core';
+import { PlaceholderComponent } from './placeholder.component';
+import { TopComponent } from './top.component';
 
 @Component({
   selector: 'app-root',
+  imports: [PlaceholderComponent, TopComponent],
   template: `
     <div class="h-screen bg-gray-500">
-      @if (topLoaded()) {
+      <!-- Trigger on condition using 'when', when the variable is set to true-->
+      @defer (when topLoaded()) {
         <app-top />
-      } @else {
+      } @placeholder (minimum 1000ms) {
         <app-placeholder />
-        <button
-          class="rounded-sm border border-blue-500 bg-blue-300 p-2"
-          (click)="topLoaded.set(true)">
-          Load Top
-        </button>
+      } @loading (minimum 1000ms) {
+        <p>Loading...</p>
       }
+      <button
+        class="rounded-sm border border-blue-500 bg-blue-300 p-2"
+        (click)="topLoaded.set(true)">
+        Load Top
+      </button>
     </div>
   `,
-  standalone: false,
 })
 export class AppComponent {
   topLoaded = signal(false);
