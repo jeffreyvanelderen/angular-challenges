8ae7523adcdb9ea87becf00465e72e4eb83eb5c1
jeffreyvanelderen <jeffrey_vanelderen@hotmail.com>
2025-04-11 11:57:42 +0200

    Add multiple content projection using ng-content to get to same result without input

--- apps/angular/57-content-projection-default/src/app/app.component.ts
+++ apps/angular/57-content-projection-default/src/app/app.component.ts
@@ -5,8 +5,15 @@ import { CardComponent } from './card.component';
   imports: [CardComponent],
   selector: 'app-root',
   template: `
-    <app-card title="Titre 1" message="Message1" />
-    <app-card title="Titre 2" />
+    <app-card>
+      <!-- De selector moet je zetten op specifieke HTML tags zoals bv div, niet random '<card-title></card-title>' want is unknown component -->
+      <div card-title>Title 1</div>
+      <div card-message>Messag 1</div>
+    </app-card>
+
+    <app-card>
+      <div card-title>Title 2</div>
+    </app-card>
   `,
   host: {
     class: 'p-4 block flex flex-col gap-1',
