db51ce06188c550cdfe4e99e736d6f8da659b179
Jeffrey Vanelderen <jeffrey.vanelderen@axxes.com>
2025-04-24 13:56:09 +0200

    Add clear explanation

--- apps/angular/32-change-detection-bug/src/app/main-navigation.component.ts
+++ apps/angular/32-change-detection-bug/src/app/main-navigation.component.ts
@@ -49,6 +49,23 @@ export class NavigationComponent {
   }
 }
 
+// The root bug was the use of a method call in the template. The code was
+// re-generating the menu items on every change detection cycle, which was
+// causing the change detection cycle to re-run each time. Each change
+// detection cycle would re-run the method, which would re-generate the menu,
+// which would cause the change detection cycle to re-run again. This would
+// continue indefinitely.
+// The solution was to build the array once in the TypeScript code and then
+// use that array in the template. This way, the array would not be
+// re-generated on each change detection cycle, and the change detection cycle
+// would not re-run indefinitely.
+// Note that changing to use signals instead of the AsyncPipe and removing the
+// extraneous ngIf layer had no effect on the behavior.
+//
+// Overall, this app is the best example I've seen of why trackBy and not
+// using method calls in the template are important.
+// https://github.com/tomalaforge/angular-challenges/pull/870/files
+
 @Component({
   imports: [NavigationComponent, NgIf],
   template: `
