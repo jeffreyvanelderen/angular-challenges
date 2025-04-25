c967996c6b28d8107631df4857e5555ee833c058
Jeffrey Vanelderen <jeffrey.vanelderen@axxes.com>
2025-04-25 11:37:44 +0200

    Answer:53 Add alternative (better) solution

--- apps/signal/53-big-signal-performance/src/app/user.service.ts
+++ apps/signal/53-big-signal-performance/src/app/user.service.ts
@@ -11,4 +11,16 @@ export class UserStore {
     zipCode: '',
     city: '',
   });
+
+  // Another (arguably better) solution would be to make readable computed fields which the other components can read from, and don't cause them to re-render:
+  // This would then still contain the big user signal
+  /*
+  readonly name = computed(() => this.user().name);
+  readonly note = computed(() => this.user().note);
+  readonly title = computed(() => this.user().title);
+  readonly salary = computed(() => this.user().salary);
+  readonly street = computed(() => this.user().address.street);
+  readonly zipCode = computed(() => this.user().address.zipCode);
+  readonly city = computed(() => this.user().address.city);
+  */
 }
