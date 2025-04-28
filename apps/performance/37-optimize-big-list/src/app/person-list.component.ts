749b2d48f5948796f4f31bffb4de71de91fb4a9f
Jeffrey Vanelderen <jeffrey.vanelderen@axxes.com>
2025-04-28 10:25:36 +0200

    Answer: 12 Improve performance of big list by using virtual rendering (only what's in in screen) using cdk virtual scroll viewport

--- apps/performance/37-optimize-big-list/src/app/person-list.component.ts
+++ apps/performance/37-optimize-big-list/src/app/person-list.component.ts
@@ -1,21 +1,23 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
-
 import { NgForTrackByModule } from '@angular-challenges/shared/directives';
-import { CommonModule } from '@angular/common';
+import { ScrollingModule } from '@angular/cdk/scrolling';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { Person } from './person.model';
 
 @Component({
   selector: 'app-person-list',
-  imports: [CommonModule, NgForTrackByModule],
+  imports: [NgForTrackByModule, ScrollingModule],
   template: `
-    <div class="relative h-[300px] overflow-hidden">
+    <div class="relative h-[500px] overflow-hidden">
       <div class="absolute inset-0 overflow-scroll">
-        <div
-          *ngFor="let person of persons; trackByProp: 'email'"
-          class="flex h-9 items-center justify-between border-b">
-          <h3>{{ person.name }}</h3>
-          <p>{{ person.email }}</p>
-        </div>
+        <!-- https://material.angular.io/cdk/scrolling/overview -->
+        <cdk-virtual-scroll-viewport appendOnly itemSize class="h-full">
+          <div
+            class="z-50 flex h-9 items-center justify-between border-b"
+            *cdkVirtualFor="let person of persons; trackBy: trackPerson">
+            <h3>{{ person.name }}</h3>
+            <p>{{ person.email }}</p>
+          </div>
+        </cdk-virtual-scroll-viewport>
       </div>
     </div>
   `,
@@ -26,4 +28,8 @@ import { Person } from './person.model';
 })
 export class PersonListComponent {
   @Input() persons: Person[] = [];
+
+  trackPerson(_: number, { email }: Person) {
+    return email;
+  }
 }
